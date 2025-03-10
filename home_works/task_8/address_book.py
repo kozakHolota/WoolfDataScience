0bb38bab531ea62c1d5a4756ec9cb1931f4dcaea
Pavlo Mryhlotskyi <kozak.holota@gmail.com>
2025-03-10 22:22:40 +0200

    Accomplish classes HW

---
+++ home_works/task_8/address_book.py
@@ -0,0 +1,123 @@
+from collections import UserDict
+from typing import List
+
+from home_works.task_8.decorators import params_handler
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    pass
+
+
+class Phone(Field):
+    def __init__(self, value: str):
+        if len(value) != 10:
+            raise ValueError("Phone number must be 10 digits")
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name: str, phones: list[Phone] = None):
+        self.name = Name(name)
+        self.phones = [] if not phones else phones
+
+    def add_phone(self, phone: str):
+        try:
+            self.phones.append(Phone(phone))
+            return "Phone added successfully"
+        except ValueError as ve:
+            return str(ve)
+
+    def remove_phone(self, phone: str):
+        try:
+            self.phones.remove(Phone(phone))
+            return "Phone removed successfully"
+        except ValueError:
+            return "Phone not found"
+
+    def edit_phone(self, old_phone: str, new_phone: str):
+        try:
+            self.phones[self.phones.index(Phone(old_phone))] = Phone(new_phone)
+            return "Phone edited successfully"
+        except ValueError:
+            return "Phone not found"
+
+    def is_phone_present(self, phone: str):
+        return Phone(phone) in self.phones
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+
+class AddressBook(UserDict):
+    command_map = {
+        "add": "add_item",
+        "find": "find",
+        "all": "get_all",
+        "remove": "remove",
+        "edit": "edit",
+    }
+    def __setitem__(self, key, value):
+        if not isinstance(value, Record):
+            raise ValueError("Value must be an instance of class Record")
+
+        self.data[key] = value
+
+    @params_handler
+    def add_item(self, name: str, *phones) -> tuple:
+        """Add a new contact to the address book
+                Usage: add <name> <phone> [<phone>] [<phone>] ..."""
+        if not phones:
+            return False, "No phones provided. At least one must be present."
+
+        try:
+            if name in self.data:
+                for phone in phones:
+                    self.data[name].add_phone(phone)
+                return True, "Contact updated successfully"
+            else:
+                self.data[name] = Record(name, [Phone(phone) for phone in phones])
+                return True, "Contact added successfully"
+        except ValueError as ve:
+            return False, "Error when add the record: " + str(ve)
+
+    @params_handler
+    def find(self, name: str) -> tuple:
+        """Find a contact in the address book by name
+                Usage: find <name>"""
+        try:
+            return True, self.data.get(name)
+        except KeyError:
+            return False, "Contact not found"
+
+    @params_handler
+    def get_all(self) -> tuple:
+        """List all contacts in the address book
+                Usage: all"""
+        return True, list(self.data.values())
+
+    @params_handler
+    def remove(self, name: str) -> tuple:
+        """Remove a contact from the address book by name
+                Usage: remove <name>"""
+        try:
+            del self.data[name]
+            return True, "Contact removed successfully"
+        except KeyError:
+            return False, "Contact not found"
+
+    @params_handler
+    def edit(self, name: str, phone: str, new_phone: str) -> tuple:
+        """Edit a phone number of a contact in the address book by name
+                Usage: edit <name> <phone> <new_phone>"""
+        try:
+            self.data[name].edit_phone(phone, new_phone)
+            return True, "Phone edited successfully"
+        except ValueError:
+            return False, "Phone not found"
---
+++ home_works/task_8/command_handler.py
@@ -0,0 +1,88 @@
+import sys
+from typing import Collection
+
+from colorama import Fore, Style, deinit
+
+from home_works.task_8.address_book import AddressBook
+from home_works.task_8.decorators import params_handler
+
+
+class CommandHandler:
+    command_map = {
+        "help": "help",
+    }
+    def __init__(self, prompt: str):
+        self.prompt = prompt
+        self.libs = [AddressBook(), self]
+
+    def __get_input(self, prompt: str) -> str:
+        return input(Fore.BLUE + prompt + Style.DIM + Style.RESET_ALL)
+
+    def __output_info(self, msg: str) -> None:
+        print(Fore.GREEN + str(msg) + Style.DIM + Style.RESET_ALL)
+
+    def __output_error(self, msg: str) -> None:
+        print(Fore.RED + str(msg) + Style.DIM + Style.RESET_ALL)
+
+
+    def __exit(self) -> None:
+        """Exits interpreter
+                        Usage: exit"""
+        acceptance = self.__get_input("Are you sure? (y/n) ")
+        if acceptance == "y":
+            self.__output_info("Goodbye!")
+            deinit()
+            sys.exit(0)
+        elif acceptance == "n":
+            self.__output_info("Okay, continue work.")
+            return
+        else:
+            self.__output_error("Wrong input. Please, try again.")
+            exit()
+
+    @params_handler
+    def help(self):
+        """Displays all available commands
+                            Usage: help"""
+        commands = ["exit"]
+        for lib in self.libs:
+            for command in lib.command_map:
+                commands.append(command)
+
+        return True, f"Available commands: {', '.join(commands)}"
+
+    def __handle_command(self, command: str):
+        command_list = command.split(' ')
+        command_name = command_list[0].lower()
+        command_args = command_list[1:] if len(command_list) > 1 else []
+
+        if command_name == "exit":
+            try:
+                self.__exit()
+            except (TypeError, ValueError) as e:
+                self.__output_error(f"Incorrect arguments to the command:\n{self.__exit.__doc__}")
+        else:
+            for lib in self.libs:
+                if command_name in lib.command_map:
+                    if command_args and command_args[0] in ["help", "h", "?"]:
+                        self.__output_info(getattr(lib, lib.command_map[command_name]).__doc__)
+                        return
+                    result, output = getattr(lib, lib.command_map[command_name])(*command_args)
+                    if result:
+                        if isinstance(output, Collection) and not isinstance(output, str):
+                            for line in output:
+                                self.__output_info(line)
+                        self.__output_info(output)
+                        return
+                    else:
+                        self.__output_error(output)
+                        return
+            self.__output_error(f"Command {command_name} not found.")
+
+    def interpreter_loop(self):
+        self.__output_info("Welcome to Contacts CMD")
+        self.__output_info("Type 'help' to get whole list of commands")
+        while True:
+            command = self.__get_input(self.prompt)
+            self.__handle_command(command)
+
