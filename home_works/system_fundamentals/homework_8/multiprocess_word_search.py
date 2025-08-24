6d3c5c3655edcaba290fdbe7a21c6dae45b54377
Pavlo Mryhlotskyi <kozak.holota@gmail.com>
2025-08-24 23:29:31 +0300

    Update homework #8: extend word search functionality to support multiple words and enhance result formatting

--- home_works/system_fundamentals/homework_8/multiprocess_word_search.py
+++ home_works/system_fundamentals/homework_8/multiprocess_word_search.py
@@ -1,17 +1,24 @@
 import multiprocessing
 import re
+from copy import copy
 from multiprocessing import Process, current_process
+from multiprocessing.managers import DictProxy
 from pathlib import Path
+from timeit import timeit
 
 import click
 
 
-def worker(files_q: multiprocessing.Queue, word: str, count: multiprocessing.Value, lock: multiprocessing.Lock):
-    """Процес-робітник: бере файли з черги та рахує входження слова."""
+def worker(
+        files_q: multiprocessing.Queue,
+        lock: multiprocessing.Lock,
+        words: tuple[str],
+        result_dict: DictProxy
+):
+    """Процес-робітник: бере файли з черги та рахує входження слів."""
     pid = current_process().pid
     name = current_process().name
 
-    pattern = re.compile(rf"\b{re.escape(word.lower())}\b")
     while True:
         try:
             file = files_q.get_nowait()
@@ -20,11 +27,19 @@ def worker(files_q: multiprocessing.Queue, word: str, count: multiprocessing.Val
         try:
             with Path(file).open("r") as f:
                 txt = f.read().lower()
-            occurrences = len(pattern.findall(txt))
-            if occurrences:
-                print(f"Слово '{word}' знайдено у файлі '{file}' {occurrences} разів. Процес {name} ({pid}).")
-            with lock:
-                count.value += occurrences
+            for word in words:
+                pattern = re.compile(r"\b" + word + r"\b")
+                occurrences = len(pattern.findall(txt))
+                if occurrences:
+                    print(f"Слово '{word}' знайдено у файлі '{file}' процесом {name}: {pid}")
+                    # Читаємо-оновлюємо-записуємо цілий об’єкт під локом
+                    with lock:
+                        d = result_dict.get(word)
+                        if d is None:
+                            d = {"findings": [], "count": 0}
+                        d["findings"].append({"path": file, "occurrences": occurrences})
+                        d["count"] += occurrences
+                        result_dict[word] = d
         except Exception:
             # Пропускаємо проблемні файли, щоб не ламати весь процес
             continue
@@ -32,8 +47,8 @@ def worker(files_q: multiprocessing.Queue, word: str, count: multiprocessing.Val
 @click.command()
 @click.option("--n", default=10, help="Number of processes")
 @click.option("--input_dir", help="Input directory")
-@click.option("-s", help="word to search")
-def main(n: int, input_dir: str, s: str):
+@click.argument("words", nargs=-1, required=True)
+def main(n: int, input_dir: str, words: tuple[str]):
     search_dir = Path(input_dir)
     if not search_dir.exists():
         raise ValueError(f"Директорії {input_dir} не існує")
@@ -41,27 +56,42 @@ def main(n: int, input_dir: str, s: str):
         raise ValueError(f"Шлях {input_dir} не директорія")
 
     files_q = multiprocessing.Queue()
+    result_dict = multiprocessing.Manager().dict()
+
     for file in search_dir.iterdir():
         if file.is_file():
             files_q.put(str(file))
 
-    count = multiprocessing.Value("i", 0)
     lock = multiprocessing.Lock()
 
     processes = []
     for _ in range(n):
-        pr = Process(target=worker, args=(files_q, s, count, lock))
+        pr = Process(target=worker, args=(files_q, lock, copy(words), result_dict))
         pr.start()
         processes.append(pr)
 
     for pr in processes:
         pr.join()
 
-    print(f"Слово '{s}' знайдено {count.value} раз(и) в папці '{input_dir}'")
-    
+    # Вивід результатів
+    for w in words:
+        data = result_dict.get(w)
+        print(10*"=")
+        print(f"Слово '{w}' по файлах")
+        print(10 * "=")
+        if data:
+            for finding in data["findings"]:
+                print(f"{finding['path']}: {finding['occurrences']}")
+            print(10 * "=")
+            print(f"Слово '{w}': {data['count']}")
+        else:
+            print("Нічого не знайдено")
+        print(10 * "=")
+
 if __name__ == "__main__":
     try:
         multiprocessing.set_start_method("spawn")
     except RuntimeError:
         pass
-    main()
+    execution_time = timeit(lambda: main(standalone_mode=False), number=1)
+    print(f"Пошук виконувався {execution_time} секунд")
--- home_works/system_fundamentals/homework_8/multithreading_word_search.py
+++ home_works/system_fundamentals/homework_8/multithreading_word_search.py
@@ -2,7 +2,9 @@ import multiprocessing
 import queue
 import re
 import threading
+from copy import copy
 from pathlib import Path
+from timeit import timeit
 
 import click
 
@@ -16,8 +18,12 @@ class Counter:
             self.value += n
 
 
-def worker(files_q: queue.Queue, word: str, count: Counter):
-    pattern = re.compile(rf"\b{re.escape(word.lower())}\b")
+def worker(
+        files_q: queue.Queue,
+        words: tuple[str],
+        result_dict: dict,
+        lock: threading.Lock
+):
     name = threading.current_thread().name
     ident = multiprocessing.current_process().ident
 
@@ -29,11 +35,19 @@ def worker(files_q: queue.Queue, word: str, count: Counter):
         try:
             with Path(file).open("r") as f:
                 txt = f.read().lower()
-            occurrences = len(pattern.findall(txt))
-            if occurrences:
-                print(f"Слово '{word}' знайдено у файлі '{file}' {occurrences} разів. Потік "
-                      f"{name} ({ident}).")
-                count.inc(occurrences)
+            for word in words:
+                pattern = re.compile(r"\b" + word + r"\b")
+                occurrences = len(pattern.findall(txt))
+                if occurrences:
+                    print(f"Слово '{word}' знайдено у файлі '{file}' потоком {name}: {ident}")
+                    # Читаємо-оновлюємо-записуємо цілий об’єкт під локом
+                    with lock:
+                        d = result_dict.get(word)
+                        if d is None:
+                            d = {"findings": [], "count": 0}
+                        d["findings"].append({"path": file, "occurrences": occurrences})
+                        d["count"] += occurrences
+                        result_dict[word] = d
         except Exception:
             continue
 
@@ -41,10 +55,12 @@ def worker(files_q: queue.Queue, word: str, count: Counter):
 @click.command()
 @click.option("--n", default=10, help="Number of processes")
 @click.option("--input_dir", help="Input directory")
-@click.option("-s", help="word to search")
-def main(n: int, input_dir: str, s: str):
+@click.argument("words", nargs=-1, required=True)
+def main(n: int, input_dir: str, words: tuple[str]):
     search_dir = Path(input_dir)
-    count = Counter()
+    result_dict = {}
+    lock = threading.Lock()
+
     if not search_dir.exists():
         raise ValueError(f"Директорії {input_dir} не існує")
     if not search_dir.is_dir():
@@ -57,14 +73,28 @@ def main(n: int, input_dir: str, s: str):
 
     threads = []
     for _ in range(n):
-        t = threading.Thread(target=worker, args=(files_q, s, count))
+        t = threading.Thread(target=worker, args=(files_q, copy(words), result_dict, lock))
         t.start()
         threads.append(t)
 
     for t in threads:
         t.join()
 
-    print(f"Слово '{s}' знайдено {count.value} раз(и) в папці '{input_dir}'")
+    # Вивід результатів
+    for w in words:
+        data = result_dict.get(w)
+        print(10 * "=")
+        print(f"Слово '{w}' по файлах")
+        print(10 * "=")
+        if data:
+            for finding in data["findings"]:
+                print(f"{finding['path']}: {finding['occurrences']}")
+            print(10 * "=")
+            print(f"Слово '{w}': {data['count']}")
+        else:
+            print("Нічого не знайдено")
+        print(10 * "=")
 
 if __name__ == "__main__":
-    main()
+    execution_time = timeit(lambda: main(standalone_mode=False), number=1)
+    print(f"Пошук виконувався {execution_time} секунд")
